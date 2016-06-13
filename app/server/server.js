21f3ec0ad21e691bd87756b4995bb54f998a1d38
domschiener <dom@fileyy.com>
2016-06-13 07:37:51 -0500

    Finished send

--- app/server/server.js
+++ app/server/server.js
@@ -1,5 +1,13 @@
 Meteor.methods({
   newOrder: function(data) {
     return Orders.insert({order: data});
+  },
+  setActive: function(orderId) {
+    return Orders.update({_id: orderId}, {
+      $set: {
+        'order.active': true,
+        'order.shipping': true
+      }
+    });
   }
 })
